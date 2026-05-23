a693249038911de311d264dc6219d82f4b161849
MeowZ#WR <antabakala@hotmail.com>
2026-05-23 21:41:21 +0800

    Restore emote image in hover tooltip after ChatLog refactor

--- ChatTwo/Ui/Handler/ChunkHandler.cs
+++ ChatTwo/Ui/Handler/ChunkHandler.cs
@@ -94,7 +94,7 @@ public class ChunkHandler
                     ImGui.Dummy(emoteSize);
 
                 if (ImGui.IsItemHovered())
-                    ImGuiUtil.Tooltip(emotePayload.Code);
+                    ImGuiUtil.EmoteTooltip(emotePayload.Code);
 
                 return;
             }
